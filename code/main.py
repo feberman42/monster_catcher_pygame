b9b32d387cd24dea4873e148d2f0c0c23b0dc408
Frederik Ebermann <feberman@student.42berlin.de>
2024-05-01 16:20:38 +0200

    animated tiles

--- code/main.py
+++ code/main.py
@@ -1,8 +1,9 @@
 from settings import *
+from support import *
 from pytmx.util_pygame import load_pygame
 from pytmx import TiledMap
 from os.path import join
-from sprites import Sprite
+from sprites import Sprite, AnimatedSprite
 from entities import Player
 from groups import AllSprites
 
@@ -16,19 +17,36 @@ class Game:
 		self.all_sprites = AllSprites()
 
 		self.import_assets()
-		self.setup(self.tmx_maps['hospital'], 'world')
+		self.setup(self.tmx_maps['world'], 'house')
 
 	def import_assets(self) -> None:
 		self.tmx_maps = {
 			'world': load_pygame(join('data', 'maps', 'world.tmx')),
 			'hospital': load_pygame(join('data', 'maps', 'hospital.tmx'))
 			}
+		
+		self.overworld_frames = {
+			'water': import_folder('graphics', 'tilesets', 'water'),
+			'coast': coast_importer(24, 12, 'graphics', 'tilesets', 'coast')
+		}
 
 	def setup(self, tmx_map: TiledMap, player_styart_position: str) -> None:
 		# terrain
 		for layer in ['Terrain', 'Terrain Top']:
 			for x, y, surf in tmx_map.get_layer_by_name(layer).tiles():
 				Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)
+
+		# water
+		for obj in tmx_map.get_layer_by_name('Water'):
+			for x in range(int(obj.x), int(obj.x + obj.width), TILE_SIZE):
+				for y in range(int(obj.y), int(obj.y + obj.height), TILE_SIZE):
+					AnimatedSprite((x, y), self.overworld_frames['water'], self.all_sprites)
+
+		# coast
+		for obj in tmx_map.get_layer_by_name('Coast'):
+			terrain = obj.properties['terrain']
+			side = obj.properties['side']
+			AnimatedSprite((obj.x, obj.y), self.overworld_frames['coast'][terrain][side], self.all_sprites)
 
 		# objects
 		for obj in tmx_map.get_layer_by_name('Objects'):
--- code/sprites.py
+++ code/sprites.py
@@ -1,3 +1,5 @@
+from typing import Any
+from pygame import Surface
 from pygame.sprite import Group
 from settings import *
 
@@ -6,3 +8,16 @@ class Sprite(pygame.sprite.Sprite):
 		super().__init__(groups)
 		self.image = surf
 		self.rect = self.image.get_frect(topleft = pos)
+
+class AnimatedSprite(Sprite):
+	def __init__(self, pos: tuple[int], frames: list[Surface], groups: Group) -> None:
+		super().__init__(pos, frames[0], groups)
+		self.frame_index = 0
+		self.frames = frames
+
+	def animate(self, dt):
+		self.frame_index += ANIMATION_SPEED * dt
+		self.image = self.frames[int(self.frame_index % len(self.frames))]
+
+	def update(self, dt) -> None:
+		self.animate(dt)
